37083e3d18cdf40367ac6b092fd218d2c5a9074d
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-12 13:38:51 +0000

    Update my-application-1.cs

--- my-application-1.cs
+++ my-application-1.cs
@@ -8,7 +8,7 @@ namespace myApplication
     { 
       // This is my message
       Console.WriteLine("This is my app and I am learning about computers");
-      string name = 'Lee Cox'; 
+      string name = "Lee Cox"; 
       Console.WriteLine(name); 
       int num = 8; 
       Console.WriteLine(num); 
