eb1996cbd6f80f510e8a04f803f6ed9c6d653bcf
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-12 17:13:38 +0000

    Create my-app-2.cs

---
+++ my-app-2.cs
@@ -0,0 +1,20 @@
+using System 
+class appTwo { 
+    static void Main() { 
+     Console.WriteLine("This is My Student"); 
+     string fname = "Shaggy "; 
+     string sname = "Cox"; 
+     string FullName = fname + sname;
+     Console.WriteLine(FullName);
+     x = 10;
+     y = 100;
+     z = 500;
+     Console.WriteLine(x / y * z);
+     PowNum = 8;
+     PowNum1 = 2; 
+     Console.WriteLine(PowNum ** PowNum1);
+
+     
+
+    }
+  }
