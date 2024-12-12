bddf70c9df382f483fabcd94ea7a868f890cc4dc
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-12 13:54:31 +0000

    Create my-application-2.cs

---
+++ my-application-2.cs
@@ -0,0 +1,12 @@
+Using System 
+  class FirstProject { 
+    static void Main() 
+    {
+    string firstname = "Lee"
+    string lastname = "Cox"
+    string FullName = firstname + lastname 
+    Console.WriteLine(FullName)
+    }
+  }
+    
+    
