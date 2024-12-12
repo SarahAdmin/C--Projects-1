792de4febb05037a962e1b96a45e715c8841a57d
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-12 13:06:37 +0000

    Create my-application-1.cs

---
+++ my-application-1.cs
@@ -0,0 +1,20 @@
+Using System; 
+
+namespace myApplication
+{
+  class Program 
+  { 
+    static void Main(string[] args) 
+    { 
+      // This is my message
+      Console.WriteLine("This is my app and I am learning about computers");
+      string name = 'Lee Cox'; 
+      Console.WriteLine(name); 
+      int num = 8; 
+      Console.WriteLine(num); 
+      bool MyBoolean = True; 
+      Console.WriteLine(MyBoolean); 
+      
+    }
+  }
+}
