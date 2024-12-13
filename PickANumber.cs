c11252e44fc7c042eaf4e729b6b7448001fe8e0d
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-13 17:35:16 +0000

    Create PickANumber.cs

---
+++ PickANumber.cs
@@ -0,0 +1,26 @@
+using System; 
+namespace LuckNum2 { 
+  class NumberWorkTwo { 
+    static void Main(string[] args) { 
+      Console.WriteLine("Enter your number: "); 
+      int num = Convert.ToInt32(Console.ReadLine()); 
+
+      switch (num);
+      case: 20: 
+      Console.WriteLine("You got a number "+num);
+      break; 
+      case: 21: 
+      case: 22: 
+      case: 23: 
+      case: 24: 
+      case: 25: 
+      Console.WriteLine("You got a number "+num); 
+      Console.WriteLine("You have a prize!");
+      break; 
+      default: 
+      ConsoleWriteLine("You got number more than 25 is "+num);
+      break;
+      
+    }
+  }
+}
