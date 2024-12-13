e878c3d13126fe8b0997f25032ae546418e34739
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-13 13:24:13 +0000

    Create String-message.cs

---
+++ String-message.cs
@@ -0,0 +1,9 @@
+using System; 
+namespace MyStringApp { 
+  class Message { 
+    static void Main(string[] args){ 
+      string text = "This is my message called Hello World"; 
+      Console.WriteLine("The length of my message"+text.Length);
+    }
+  }
+}
