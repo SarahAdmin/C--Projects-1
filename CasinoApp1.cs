0dd50e31d8002cbe76fb8815b7adbc02097c9f76
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-13 13:51:26 +0000

    Create CasinoApp1.cs

---
+++ CasinoApp1.cs
@@ -0,0 +1,13 @@
+using System; 
+namespace CasinoApp { 
+  class gameOne { 
+    static void Main(string[] args) { 
+      Console.ReadLine("Enter your name:  ");
+      string name = Console.ReadLine(); 
+      Console.WriteLine("Enter your lucky number: "); 
+      int luckNum = ToInt32(Console.ReadLine()); 
+      Console.WriteLine("Name: "+name); 
+      Console.WriteLine("Lucky Number:"+luckNum); 
+    }
+  }
+}
