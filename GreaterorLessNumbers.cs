171395fc1aa7a7ac2fb1484233070d24aae91ec5
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-13 16:28:39 +0000

    Create GreaterorLessNumbers.cs

---
+++ GreaterorLessNumbers.cs
@@ -0,0 +1,14 @@
+using System; 
+namespace NumberWork1 {
+  class ProgramOne {
+    static void Main(string[] args) {
+      num1 = 20;
+      num2 = 5; 
+
+      Console.WriteLine(num1 < num2); 
+      Console.WriteLine(num1 > num2); 
+      
+    }
+  }
+}
+    
