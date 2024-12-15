b66c5c77b6c22658024cb25bb2f8994cb064d03f
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-15 23:02:33 +0000

    Create CountEven.cs

---
+++ CountEven.cs
@@ -0,0 +1,10 @@
+using System; 
+namespace CountOne { 
+  class CountEvenNumbers { 
+    static void Main(string[] args) {
+      for(int i; i <=21; i = i + 2) {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
