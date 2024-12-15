74cb6189193de7afff1a20092bd7de0d6e48a6cb
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-15 22:19:27 +0000

    Create CountTwenty.cs

---
+++ CountTwenty.cs
@@ -0,0 +1,12 @@
+using System; 
+namespace MyNumbers1 { 
+  class CountToTwenty { 
+    static void Main(string[] args) { 
+      int i = 1; 
+      while (i < 21) {
+        Console.WriteLine(i); 
+        i++;
+        }
+    }
+  }
+}
