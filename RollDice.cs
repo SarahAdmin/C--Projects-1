3f37eca2f5984b8a356858122bb42fcd14154316
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-17 21:00:53 +0000

    Create RollDice.cs

---
+++ RollDice.cs
@@ -0,0 +1,38 @@
+using System; 
+namespace Game1 {
+  class RollADice { 
+    static void Main(string[] args) {
+      Random rgame = new Random(); 
+      int dice = rgame.Next(1,6); 
+      switch(dice) { 
+        case 1: 
+        Console.WriteLine(dice);
+        Console.WriteLine("You rolled Number 1");
+        break; 
+        case 2: 
+        Console.WriteLine(dice);
+        Console.WriteLine("You rolled Number 2");
+        break; 
+        case 3: 
+        Console.WriteLine(dice);
+        Console.WriteLine("You rolled Number 3");
+        break; 
+        case 4: 
+        Console.WriteLine(dice);
+        Console.WriteLine("You rolled Number 4");
+        break; 
+        case 5: 
+        Console.WriteLine(dice);
+        Console.WriteLine("You rolled Number 5");
+        break; 
+        case 6: 
+        Console.WriteLine(dice);
+        Console.WriteLine("You rolled Number 6");
+        break;
+        default: 
+        Console.WriteLine("No Number");
+        break;
+      }
+    }
+  }
+}
