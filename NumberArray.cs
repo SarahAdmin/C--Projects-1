ffe5a6208da2f8d8f6b68d523a71c0de66b4adef
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-18 16:06:26 +0000

    Update NumberArray.cs

--- NumberArray.cs
+++ NumberArray.cs
@@ -2,7 +2,7 @@ using System;
 namespace ArrApplication { 
   class NumberArray { 
     static void Main(string[] args) {
-      int num[] = {82,45,12,54,69,48,34,65,76,34,21,65,9}; 
+      int[] num = {82,45,12,54,69,48,34,65,76,34,21,65,9}; 
       Console.WriteLine(num.Max());
       Console.WriteLine(num.Min());
       Console.WriteLine(num.Sum());
