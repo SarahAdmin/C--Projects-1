b3664c7874e998b960109398817fc039afe2677b
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-21 14:24:25 +0000

    Create MyMethodOne.cs

---
+++ MyMethodOne.cs
@@ -0,0 +1,12 @@
+using System; 
+namespace myApp { 
+  class MyMethodOne { 
+    static void Message1() { 
+      Console.WriteLine("Hello World"); 
+    } 
+   static void Main(string[] args) {
+     Message1()
+
+}
+  }
+   }
