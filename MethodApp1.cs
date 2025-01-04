b90d62cbe6700a8d322ffc613e3a7e046d49813f
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-04 18:43:59 +0000

    Create MethodApp1.cs

---
+++ MethodApp1.cs
@@ -0,0 +1,10 @@
+using System; 
+namespace MyAppSix { 
+  class MethodApp1 { 
+    static void MyMessage() { 
+      Console.WriteLine("This is my method");
+      static void Main (string[] args) { 
+        MyMessage();
+      }
+    }
+  }
