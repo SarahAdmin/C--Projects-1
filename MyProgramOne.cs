278a20bf6d9e15c55adb300f60b6737313d44eae
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-04 18:07:54 +0000

    Update MyProgramOne.cs

--- MyProgramOne.cs
+++ MyProgramOne.cs
@@ -3,12 +3,12 @@ namespace MyObjects {
   class MyProgramOne { 
     static void Main(string[] args) { 
       Car Toyota = new Car(); 
-      Toyota.model = 'Yaris'; 
-      Toyota.colour = 'Black'; 
+      Toyota.model = "Yaris"; 
+      Toyota.colour = "Black"; 
       Toyota.year = 2019; 
       Car Ford = new Car(); 
-      Ford.model = 'Fiesta'; 
-      Ford.colour = 'Blue'; 
+      Ford.model = "Fiesta"; 
+      Ford.colour = "Blue"; 
       Ford.year = 2023; 
 
       Console.WriteLine("Model: "+Ford.model); 
