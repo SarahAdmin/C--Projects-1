da12746e964157e02456e9cf85136f22d82483bc
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-04 18:05:18 +0000

    Create MyProgramOne.cs

---
+++ MyProgramOne.cs
@@ -0,0 +1,19 @@
+using System; 
+namespace MyObjects { 
+  class MyProgramOne { 
+    static void Main(string[] args) { 
+      Car Toyota = new Car(); 
+      Toyota.model = 'Yaris'; 
+      Toyota.colour = 'Black'; 
+      Toyota.year = 2019; 
+      Car Ford = new Car(); 
+      Ford.model = 'Fiesta'; 
+      Ford.colour = 'Blue'; 
+      Ford.year = 2023; 
+
+      Console.WriteLine("Model: "+Ford.model); 
+      Console.WriteLine("Colour: "+Ford.colour);
+      Console.WriteLine("Year: "+Ford.year);
+    }
+  }
+}
