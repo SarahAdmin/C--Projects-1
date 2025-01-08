d230a42e73725f6fd31a4dcb9c01f9e86c5a49c3
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-08 17:06:16 +0000

    Create FindAMax.cs

---
+++ FindAMax.cs
@@ -0,0 +1,11 @@
+using System; 
+namespace NumWork2 { 
+  class MaxNumbers { 
+    static int FindAMax(int num1, int num2) { 
+      return Math.Max(num1,num2);
+    }
+    static void (string[] args) { 
+      Console.WriteLine(FindAMax(48,285);
+    }
+  }
+}
