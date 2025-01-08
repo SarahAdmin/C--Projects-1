436f3127d0e7a8b5d53a5883534ed3e9212ff127
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-08 13:10:08 +0000

    Create JellyMethod.cs

---
+++ JellyMethod.cs
@@ -0,0 +1,14 @@
+using System; 
+namespace MyApplicationEight { 
+  class MyAppilcation2 { 
+    static void MyJellies(flavour) { 
+      Console.WriteLine(flavour + "Jelly");
+    }
+    static void Main(string[] args) { 
+      MyJellies("Lemon");
+      MyJellies("Strawberry");
+      MyJellies("Raspberry");
+      MyJellies("Orange"); 
+    }
+  }
+}
