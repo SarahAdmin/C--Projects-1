0c7ad61807b1d31608b8596033713d2668c2ffda
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-08 13:58:06 +0000

    Update PowNumbers.cs

--- PowNumbers.cs
+++ PowNumbers.cs
@@ -1,7 +1,7 @@
 using System; 
 namespace NumApp { 
   class PowNumbers { 
-    static void MySquareNum(int num) { 
+    static double MySquareNum(int num) { 
       return Math.Sqrt(num); 
       }
     static void Main(string[] args) { 
