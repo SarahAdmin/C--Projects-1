a569e7778481eaa8f22c381ccfc7f11fd631e79f
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-08 13:04:25 +0000

    Update MethodApp1.cs

--- MethodApp1.cs
+++ MethodApp1.cs
@@ -3,6 +3,7 @@ namespace MyAppSix {
   class MethodApp1 { 
     static void MyMessage() { 
       Console.WriteLine("This is my method");
+    }
       static void Main (string[] args) { 
         MyMessage();
       }
